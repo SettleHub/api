968ff2506c44fd061777d16e39bcc67833dfd705
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-05-22 21:02:53 +0000

    add booking service with eureka client implementation

---
+++ services/booking/src/test/java/org/settlehub/booking/BookingServiceTests.java
@@ -0,0 +1,13 @@
+package org.settlehub.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
