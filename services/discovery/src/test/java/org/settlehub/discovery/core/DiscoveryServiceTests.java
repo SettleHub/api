84e73f4d1bda231bb84f8e3d8bb03df40fa8a65b
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-05-22 16:41:38 +0000

    rename test package

--- services/discovery/src/test/java/org/settlehub/discovery/core/DiscoveryServiceTests.java
+++ services/discovery/src/test/java/org/settlehub/discovery/core/DiscoveryServiceTests.java
@@ -1,13 +1,14 @@
-package org.settlehub.discovery;
+package org.settlehub.discovery.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServiceApplicationTests {
+class DiscoveryServiceTests {
 
 	@Test
 	void contextLoads() {
 	}
 
 }
+
