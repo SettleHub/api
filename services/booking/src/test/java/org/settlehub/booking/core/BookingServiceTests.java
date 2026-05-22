2969c150270245803cfd0a79dab7cf184f64e835
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-05-23 02:42:57 +0300

    fix actions script tree and trigger services CI/CD (#6)
    * fix actions script tree and trigger services CI/CD
    
    * fix test typing in notification and booking services
    
    * fix test application-test.yml for notification service
    
    * fix test application-test.yml for booking service
    
    * fix importing
    
    * fix test score application-test.yml
    
    * update github scripts with manual reviews

--- services/booking/src/test/java/org/settlehub/booking/core/BookingServiceTests.java
+++ services/booking/src/test/java/org/settlehub/booking/core/BookingServiceTests.java
@@ -1,13 +1,14 @@
-package org.settlehub.booking;
+package org.settlehub.booking.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BookingServiceTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() { }
 
 }
---
+++ services/notification/src/test/java/org/settlehub/notification/core/NotificationServiceTests.java
@@ -0,0 +1,14 @@
+package org.settlehub.notification.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class NotificationServiceTests {
+
+	@Test
+	void contextLoads() { }
+
+}
