09bd2becee67982b5719ce15f74dc70b659659e7
Vladyslav Karakai <hitechnic00074220@gmail.com>
2026-05-24 18:58:15 +0000

    fix application-test.yml

--- services/api-gateway/src/test/java/org/settlehub/gateway/GatewayApplicationTests.java
+++ services/api-gateway/src/test/java/org/settlehub/gateway/GatewayApplicationTests.java
@@ -2,12 +2,13 @@ package org.settlehub.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GatewayApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+	void contextLoads() { }
 
 }
